d16adbfb943d136f0dcce96b55f532c24e4d8db3
Karol Rosiński <krosinski94@gmail.com>
2023-04-13 23:57:39 +0200

    add tests descriptions header

---
+++ cypress/e2e/specs/shared/header.cy.js
@@ -0,0 +1,19 @@
+describe('header-tests', () => {
+    it('logo navigates on click to /', () => {
+
+    })
+
+    context('hamburger icon on different viewports', () => {
+        it('appears on iphone 6+', () => {
+
+        })
+
+        it('appears on ipad 2', () => {
+            
+        })
+
+        it('does not appear on macbook 13', () => {
+            
+        })
+    })
+})
--- cypress/e2e/specs/users/logout.cy.js
+++ cypress/e2e/specs/users/logout.cy.js
@@ -3,7 +3,7 @@ describe('logout-tests', () => {
 
     })
 
-    it('change information and button on successful logout', () => {
+    it('changes information and button on successful logout', () => {
         
     })
 })
