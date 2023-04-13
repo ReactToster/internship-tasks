e6d3bb6e146de5b61a7ecdadb6a5764a16e34cee
Karol Rosiński <krosinski94@gmail.com>
2023-04-13 23:35:22 +0200

    add tests descriptions login

---
+++ cypress/e2e/specs/users/login.cy.js
@@ -0,0 +1,29 @@
+describe('login-tests', () => {
+    context('validating web elements', () => {
+        it('title Sample App', () => {
+
+        })
+
+        it('fill in form informations', () => {
+            
+        })
+
+        it('username has placeholder text', () => {
+            
+        })
+
+        it('password has placeholder text', () => {
+            
+        })
+    })
+
+    context('logging actions', () => {
+        it('incorrect password and username', () => {
+
+        })
+
+        it('navigates to logout on successful login', () => {
+            
+        })
+    })
+})
